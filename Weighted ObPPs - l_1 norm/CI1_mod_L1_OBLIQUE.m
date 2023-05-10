5c83d27c46ec573251540b4bb41fe5aff6ec157a
TereziaF <50276989+TereziaF@users.noreply.github.com>
2023-05-10 15:45:09 +0200

    Add files via upload

---
+++ Weighted ObPPs - l_1 norm/CI1_mod_L1_OBLIQUE.m
@@ -0,0 +1,24 @@
+function[X,t_var,S,V,G] = CI1_mod_L1_OBLIQUE(U,C,A,B,W,m,n,p,q,gamma)
+
+cvx_begin
+variable X(m,n) 
+variable G(n,n) symmetric
+variable t_var(1,1)
+variable S(p,q)
+variable V(m+n, m+n) symmetric
+minimize sum(diag(U*V))
+
+diag(G) == ones(n,1);
+
+V == [eye(m), X; X', G];
+
+V == semidefinite(m+n);
+
+-S <= W.*(C-A*X*B) <= S;
+
+S'*ones(p,1) <= t_var*ones(q,1);
+
+t_var <= gamma;
+
+cvx_end
+end
---
+++ Weighted ObPPs - l_1 norm/SDPrelax_OBLIQUE_L1.m
@@ -0,0 +1,23 @@
+function[X,t,S,V,G] = SDPrelax_OBLIQUE_L1(A, B, C, W, m, n, p, q)
+
+cvx_begin
+variable X(m,n) 
+variable G(n,n) symmetric
+variable t(1,1)
+variable S(p,q)
+variable V(m+n, m+n) symmetric
+minimize t
+
+diag(G) == ones(n,1);
+
+V == [eye(m), X; X', G];
+
+V == semidefinite(m+n);
+
+-S <= W.*(C-A*X*B) <= S;
+
+S'*ones(p,1) <= t*ones(q,1);
+
+cvx_end
+
+end
