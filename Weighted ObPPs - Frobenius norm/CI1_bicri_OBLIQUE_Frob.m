b0401856d481d2bf4cc2eeffd1c6a88d9b54f6f9
TereziaF <50276989+TereziaF@users.noreply.github.com>
2023-05-22 18:05:09 +0200

    CI1_bicri_OBLIQUE_Frob.m

--- Weighted ObPPs - Frobenius norm/CI1_bicri_OBLIQUE_Frob.m
+++ Weighted ObPPs - Frobenius norm/CI1_bicri_OBLIQUE_Frob.m
@@ -1,5 +1,16 @@
 function[X,V,G,Y,Z] = CI1_bicri_OBLIQUE_Frob(U,C,A,B,W,m,n,p,q,alpha)
 
+%inputs:
+%U - output of CI1_...
+%A,B,C - data of the given problem
+%W - matrix specifying missing elements of C
+%m,n,p,q - dimensions of data
+%alpha - relative weight for bi-criterion problem
+
+%outputs:
+%X - orthogonal solution
+%Y,Z,V,G - solutions of the reformulated problem
+
 cvx_begin sdp
 variable X(m,n) 
 variable G(n,n) symmetric
@@ -7,6 +18,8 @@ variable Y(p+q,p+q) symmetric
 variable V(m+n, m+n) symmetric
 variable Z(p,p) symmetric
 minimize sum(diag(Z)) + alpha*sum(diag(U*V))
+
+%INSERT ADDITIONAL LINEAR OR SEMIDEFINITE CONSTRAINTS HERE
 
 Y == [eye(q), W'.*(C - A*X*B)'; W.*(C - A*X*B), Z];
 
