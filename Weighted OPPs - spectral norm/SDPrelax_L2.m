f9b7164f243ff0f5d8b3bc586db14d6893781b31
TereziaF <50276989+TereziaF@users.noreply.github.com>
2023-05-22 17:38:11 +0200

    SDPrelax_L2.m

--- Weighted OPPs - spectral norm/SDPrelax_L2.m
+++ Weighted OPPs - spectral norm/SDPrelax_L2.m
@@ -1,4 +1,12 @@
 function[X,Y,s,V] = SDPrelax_L2(A, B, C, W, m, n, p, q)
+  
+%inputs:
+%A,B,C - data of the given problem
+%m,n,p,q - dimensions of data
+
+%outputs:
+%X - orthogonal solution
+%Y,s,V - solutions of the reformulated problem
 
 cvx_begin sdp
 variable X(m,n) 
@@ -6,6 +14,8 @@ variable s(1,1)
 variable Y(q+p, q+p) symmetric
 variable V(m+n, m+n) symmetric
 minimize s
+
+%INSERT ADDITIONAL LINEAR OR SEMIDEFINITE CONSTRAINTS HERE
 
 Y == [s*eye(p), W.*(C - A*X*B); W'.*(C - A*X*B)', s*eye(q)];
 
