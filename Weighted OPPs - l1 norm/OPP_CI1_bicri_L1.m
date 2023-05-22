8685906e0670db8f00678c97ef0e7b470afccb80
TereziaF <50276989+TereziaF@users.noreply.github.com>
2023-05-22 16:56:05 +0200

    OPP_CI1_bicri_L1.m

--- Weighted OPPs - l1 norm/OPP_CI1_bicri_L1.m
+++ Weighted OPPs - l1 norm/OPP_CI1_bicri_L1.m
@@ -1,4 +1,15 @@
 function[X,t_var,S,V] = OPP_CI1_bicri_L1(U,C,A,B,W,m,n,p,q,alpha)
+  
+%inputs:
+%U - output of OPP_CI2
+%A,B,C - data of the given problem
+%W - matrix specifying missing elements of C
+%m,n,p,q - dimensions of data
+%alpha - relative weight for bi-criterion problem
+
+%outputs:
+%X - orthogonal solution
+%Y,Z,V - solutions of the reformulated problem
 
 cvx_begin
 variable X(m,n) 
@@ -6,6 +17,8 @@ variable t_var(1,1)
 variable S(p,q)
 variable V(m+n, m+n) symmetric
 minimize t_var + alpha*sum(diag(U*V))
+  
+%INSERT ADDITIONAL LINEAR OR SEMIDEFINITE CONSTRAINTS HERE
 
 V == [eye(m), X; X', eye(n)];
 
