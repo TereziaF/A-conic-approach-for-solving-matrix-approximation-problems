a5ddc74a888f62814100a00cab29a49305cc6c4a
TereziaF <50276989+TereziaF@users.noreply.github.com>
2023-05-22 17:18:19 +0200

    SDPrelax_Linfty.m

--- Weighted OPPs - l_infty norm/SDPrelax_Linfty.m
+++ Weighted OPPs - l_infty norm/SDPrelax_Linfty.m
@@ -1,11 +1,21 @@
 function[X,t,S,V] = SDPrelax_Linfty(A, B, C, W, m, n, p, q)
 
+%inputs:
+%A,B,C - data of the given problem
+%m,n,p,q - dimensions of data
+
+%outputs:
+%X - orthogonal solution
+%t,S,V - solutions of the reformulated problem
+
 cvx_begin
 variable X(m,n) 
 variable t(1,1)
 variable S(p, q)
 variable V(m+n, m+n) symmetric
 minimize t
+
+%INSERT ADDITIONAL LINEAR OR SEMIDEFINITE CONSTRAINTS HERE
 
 V == [eye(m), X; X', eye(n)];
 
