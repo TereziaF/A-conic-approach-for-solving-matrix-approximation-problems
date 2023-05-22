b4ebf9544496c48ee60c02a4f53a863287281d4e
TereziaF <50276989+TereziaF@users.noreply.github.com>
2023-05-22 16:39:36 +0200

    SDPrelax_L1.m

--- Weighted OPPs - l1 norm/SDPrelax_L1.m
+++ Weighted OPPs - l1 norm/SDPrelax_L1.m
@@ -1,4 +1,13 @@
 function[X,t,S,V] = SDPrelax_L1(A, B, C, W, m, n, p, q)
+  
+%inputs:
+%A,B,C - data of the given problem
+%W - matrix specifying missing entries of C
+%m,n,p,q - dimensions of data
+
+%outputs:
+%X - orthogonal solution
+%t,S,V - solutions of the reformulated problem
 
 cvx_begin
 variable X(m,n) 
@@ -6,6 +15,8 @@ variable t(1,1)
 variable S(p, q)
 variable V(m+n, m+n) symmetric
 minimize t
+
+%INSERT ADDITIONAL LINEAR OR SEMIDEFINITE CONSTRAINTS HERE
 
 V == [eye(m), X; X', eye(n)];
 
