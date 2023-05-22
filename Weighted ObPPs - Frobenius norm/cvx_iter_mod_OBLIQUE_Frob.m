5e03e905653af54a638d288072941aa5f26ca2fc
TereziaF <50276989+TereziaF@users.noreply.github.com>
2023-05-22 18:01:02 +0200

    cvx_iter_mod_OBLIQUE_Frob.m

--- Weighted ObPPs - Frobenius norm/cvx_iter_mod_OBLIQUE_Frob.m
+++ Weighted ObPPs - Frobenius norm/cvx_iter_mod_OBLIQUE_Frob.m
@@ -1,4 +1,28 @@
 function[X,hodnost,hodnost_final,g,g_final,norma,norm_final,cas,t,s,empirical_epsilon,sum_eig_final,sum_eig_real,V,G,Y,Z] = cvx_iter_mod_OBLIQUE_Frob(C,A,B,W,k,epsilon,M,alpha)
+ 
+%inputs:
+% C, A, B - data of the problem
+% W - matrix specifying missing elements of C
+% k - desired rank
+% epsilon - tolerance
+% M - maximum number of the same iterations
+% gamma - upper bound on the objective value
+
+%outputs:
+% X - oblique solution
+% hodnost - rank of variable V in all iterations
+% hodnost_final - rank of solution V
+% g - values of the objective in all iterations
+% g_final - optimal value of the reformulated problem
+% norma - values of the original objective in all iterations
+% norm_final - optimal value of the original problem
+% cas - computation time
+% t - number of iterations
+% s - number of "same-rank" iterations
+% empirical_epsilon - value of the k-largest eigenvalue
+% sum_eig_final - optimal value of OPP_CI2 in final iteration
+% sum_eig_real - sum of (m+n-k) smallest eigenvalues of solution V
+% Z, Y, V, G - solutions of the reformulated problem
 
 %dimensions
 p = size(C,1);
@@ -24,6 +48,7 @@ t=0;
 %set counter of the same iterations
 s=0;
 
+%algorithm
 while (hodnost(end) > k && s < M)
 
 t = t+1;
@@ -44,6 +69,7 @@ end
 
 end
 
+%specifying outputs
 vh = eig(V);
 empirical_epsilon = vh(n+m-k);
 g_final = sum(diag(Z));
