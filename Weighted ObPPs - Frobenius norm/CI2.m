bce96f81d063c7ff671fa2b3c89dacc3fce12a99
TereziaF <50276989+TereziaF@users.noreply.github.com>
2023-05-22 18:04:19 +0200

    CI2.m

--- Weighted ObPPs - Frobenius norm/CI2.m
+++ Weighted ObPPs - Frobenius norm/CI2.m
@@ -1,5 +1,13 @@
 function[U] = CI2(V,m,n,k)
 
+%inputs:
+%V - output of CI1_...
+%m,n - dimensions of data
+%k - desired rank
+
+%outputs:
+%U - direction matrix for next iteration of the algorithm
+
 cvx_begin 
 variable U(n+m,n+m) symmetric
 minimize sum(diag(U*V))
